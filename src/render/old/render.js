68dcc6529af3e2db8e5cd906bcb9faaa996d859f
xieyucheng <648547525@qq.com>
2019-03-24 19:51:02 +0800

    delete old folder

--- src/render/old/render.js
+++
@@ -1,8 +0,0 @@
-
-ipcRenderer.on('save_success',function(err,data){
-    alert('保存成功');
-})
-
-ipcRenderer.on('replycreate',function(err,data){
-    addr.innerText='地址:'+data;
-})
--- src/render/old/test.js
+++
@@ -1,7 +0,0 @@
-// Kyph8rzUPCpndJfRvFSpQ5TWApHbjRZQy7AE4oiAc2xczyszTgx6
-
-const bip32 = require('bip32')
-
-let Bip32=bip32.fromBase58('Kyph8rzUPCpndJfRvFSpQ5TWApHbjRZQy7AE4oiAc2xczyszTgx6');
-
-console.log(Bip32);
